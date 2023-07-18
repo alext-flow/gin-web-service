8654f8ded35a1e8aad8c2107a09b1a9f2dba691b
alex tal <alex.tal@flowsecurity.com>
2023-07-18 12:21:18 +0300

    refactor: refactor according to best practice

--- api/main.go
+++ api/main.go
@@ -1,62 +1,15 @@
 package main
 
 import (
+	"api/mocks"
+	"common/models"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"
-	"encoding/json"
 )
 
-type album struct {
-	ID     string    `json:"id"`
-	Title  string    `json:"title"`
-	Artist string    `json:"artist"`
-	Price  float64   `json:"price"`
-	Type   albumType `json:"type"`
-}
-
-type albumType struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	Desc string `json:"desc"`
-}
-
-var albums = []album{
-	{
-		ID:     "1",
-		Title:  "Blue Train",
-		Artist: "John Coltrane",
-		Price:  56.99,
-		Type: albumType{
-			ID:   "vinyl-1",
-			Name: "vinyl",
-			Desc: "oldest and round",
-		},
-	},
-	{
-		ID:     "2",
-		Title:  "Jeru",
-		Artist: "Gerry Mulligan",
-		Price:  17.99,
-		Type: albumType{
-			ID:   "cd-1",
-			Name: "cd",
-			Desc: "old, round and relatively small",
-		},
-	},
-	{
-		ID:     "3",
-		Title:  "Sarah Vaughan and Clifford Brown",
-		Artist: "Sarah Vaughan",
-		Price:  39.99,
-		Type: albumType{
-			ID:   "minidisc-1",
-			Name: "minidisc",
-			Desc: "round, small and the biggest flop of all",
-		},
-	},
-}
-
-var albumTypes map[string]albumType = map[string]albumType{
+var albumTypes map[string]models.AlbumType = map[string]models.AlbumType{
 	"vinyl": {
 		ID:     "vinyl-1",
 		Name:  "vinyl",
@@ -74,8 +27,8 @@ var albumTypes map[string]albumType = map[string]albumType{
 	},
 }
 
-func postAlbums(c *gin.Context) {
-    var newAlbum album
+func createAlbum(c *gin.Context) {
+    var newAlbum models.Album
 
     // bind the received JSON to newAlbum
 	if err := c.ShouldBindJSON(&newAlbum); err != nil {
@@ -88,52 +41,52 @@ func postAlbums(c *gin.Context) {
     }
 
     // add the new album to the slice
-    albums = append(albums, newAlbum)
+    mocks.Albums = append(mocks.Albums, newAlbum)
     c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
 
 // getAlbums responds with the list of all albums as JSON
 func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
+    c.IndentedJSON(http.StatusOK, mocks.Albums)
 }
 
-func getType(c *gin.Context){
+func getAlbumType(c *gin.Context){
 	albumType := c.Query("type")
-	typeName, ok := albumTypes[albumType]
-	if !ok{
-		c.JSON(http.StatusNotFound, gin.H{
-			"error": "Invalid album type",
-		})
+	//typeName, ok := albumTypes[albumType]
+	if typeName, ok := albumTypes[albumType]; ok{
+		c.JSON(http.StatusOK, typeName)
 		return
 	}
-	c.JSON(http.StatusOK, typeName)
+	c.AbortWithError(http.StatusNotFound, fmt.Errorf("invalid album type"))
+	// c.JSON(http.StatusNotFound, gin.H{
+	// 	"error": "Invalid album type",
+	// })
 }
 
 func getAlbumByID(c *gin.Context) {
     id := c.Param("id")
 
 	// traverse through albums, find the album by id
-	for _, a := range albums {
-        if a.ID == id {
-            c.IndentedJSON(http.StatusOK, a)
+	for _, album := range mocks.Albums {
+        if album.ID == id {
+            c.IndentedJSON(http.StatusOK, album)
             return
         }
     }
     c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
-func deleteById(c *gin.Context){
-	id := c.Param("id")
+func deleteAlbumById(c *gin.Context){
+	albumID := c.Param("id")
 
-	for index, album := range albums {
-		if album.ID == id {
-			deletedAlbum := album
+	for index, album := range mocks.Albums {
+		if album.ID == albumID {
 
 			// delete from the "db" by getting all albums before all albums after
-			albums = append(albums[:index], albums[index+1:]...)
+			mocks.Albums = append(mocks.Albums[:index], mocks.Albums[index+1:]...)
 
-			c.JSON(http.StatusOK, deletedAlbum)
+			c.JSON(http.StatusOK, album)
 			return
 		}
 	}
@@ -144,10 +97,10 @@ func getAlbumsByType(c *gin.Context) {
     typeName := c.Param("type")
 	
 	// create a new slice to store albums of the specified type
-	var albumsOfType []album
+	var albumsOfType []models.Album
 
     // iterate over the list of albums
-	for _, a := range albums {
+	for _, a := range mocks.Albums {
 		// If the album type matches the specified type, add it to the slice
         if a.Type.Name == typeName {
 			albumsOfType = append(albumsOfType, a)
@@ -164,7 +117,7 @@ func getAlbumsByType(c *gin.Context) {
 	c.JSON(http.StatusOK, albumsOfType)
 }
 
-func patchAlbum(c *gin.Context) {
+func updateAlbum(c *gin.Context) {
 	id := c.Param("id")
 	var patchData map[string]interface{}
 
@@ -174,7 +127,7 @@ func patchAlbum(c *gin.Context) {
 	}
 
 	// find the album to patch
-	for index, album := range albums {
+	for index, album := range mocks.Albums {
 		if album.ID == id {
 			patchedAlbum := album
 			patchDataBytes, _ := json.Marshal(patchData)
@@ -183,7 +136,7 @@ func patchAlbum(c *gin.Context) {
 				return
 			}
 
-			albums[index] = patchedAlbum
+			mocks.Albums[index] = patchedAlbum
 			c.JSON(http.StatusOK, patchedAlbum)
 			return
 		}
@@ -195,12 +148,12 @@ func patchAlbum(c *gin.Context) {
 func main() {
 	router := gin.Default()
   
-	router.GET("/type", getType);
+	router.GET("/type/:type", getAlbumType);
 	router.GET("/albums", getAlbums)
-	router.POST("/albums", postAlbums)
+	router.POST("/albums", createAlbum)
 	router.GET("/albums/:id", getAlbumByID)
-	router.DELETE("/albums/:id", deleteById)
+	router.DELETE("/albums/:id", deleteAlbumById)
 	router.GET("/albums/type/:type", getAlbumsByType)
-	router.PATCH("/albums/:id", patchAlbum)
+	router.PATCH("/albums/:id", updateAlbum)
 	router.Run()
   }
